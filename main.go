4a85bd507067da1ed57e94433e94eda77b4e5d12
bingoohuang <bingoo.huang@gmail.com>
2020-08-04 13:00:36 +0800

    repeat log

--- main.go
+++ main.go
@@ -7,17 +7,21 @@ import (
 )
 
 func main() {
-	log.Print("env TZ:", os.Getenv("TZ"))
-	log.Print("time.Local:", time.Local)
-	log.Println("Local Time:", time.Now())
-	log.Println("UTC Time:", time.Now().UTC())
-	log.Println("Unix Seconds:", time.Now().Unix())
-	log.Println("UnixNano:", time.Now().UnixNano())
-
 	log2 := log.New(os.Stderr, "UTC ", log.LstdFlags|log.LUTC)
-	log2.Print("time.Local:", time.Local)
-	log2.Println("Local Time:", time.Now())
-	log2.Println("UTC Time:", time.Now().UTC())
-	log2.Println("Unix Seconds:", time.Now().Unix())
-	log2.Println("UnixNano:", time.Now().UnixNano())
+
+	c := time.NewTicker(10 * time.Second).C
+	for ; ; <-c {
+		log.Print("env TZ:", os.Getenv("TZ"))
+		log.Print("time.Local:", time.Local)
+		log.Print("Local Time:", time.Now())
+		log.Print("UTC Time:", time.Now().UTC())
+		log.Print("Unix Seconds:", time.Now().Unix())
+		log.Print("UnixNano:", time.Now().UnixNano())
+
+		log2.Print("time.Local:", time.Local)
+		log2.Print("Local Time:", time.Now())
+		log2.Print("UTC Time:", time.Now().UTC())
+		log2.Print("Unix Seconds:", time.Now().Unix())
+		log2.Print("UnixNano:", time.Now().UnixNano())
+	}
 }
